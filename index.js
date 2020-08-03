c83f7d0101d0ec94cc68383467932e5c45300c8c
niteshseram <niteshseram@gmail.com>
2020-08-03 20:25:42 +0530

    final deployment

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/logging")();
 require("./startup/config")();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
